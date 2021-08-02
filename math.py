2705a307e65e883a154088566fda0f855cffceba
sujata-chavan <71012432+sujata-chavan@users.noreply.github.com>
2021-08-02 14:30:58 +0530

    Initial Commit of math.py

---
+++ math.py
@@ -0,0 +1,12 @@
+#add implementation
+def add(x,y):
+    pass
+#sub implementation
+def subtract(x,y):#on master branch
+    return x-y
+#mul implementation
+def multiply(x,y):
+    return x*y     #on bug456
+#div implementation
+def divide(x,y):
+    pass
